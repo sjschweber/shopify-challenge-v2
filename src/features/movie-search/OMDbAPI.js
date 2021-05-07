cdc90647f77985b273bfcb617f1107ece43e42e5
sjschweber <schweber97@gmail.com>
2021-05-06 21:05:35 -0400

    Add https

--- src/features/movie-search/OMDbAPI.js
+++ src/features/movie-search/OMDbAPI.js
@@ -3,6 +3,5 @@ require('dotenv').config({path: '../../../.env'})
 const axios = require('axios')
 
 export function fetchMovieTitles(movieTitle) {
-  console.log(process.env.REACT_APP_API_KEY)
-  return axios.get(`http://www.omdbapi.com/?s=${movieTitle}&apikey=${process.env.REACT_APP_API_KEY}`)
+  return axios.get(`https://www.omdbapi.com/?s=${movieTitle}&apikey=${process.env.REACT_APP_API_KEY}`)
 }
