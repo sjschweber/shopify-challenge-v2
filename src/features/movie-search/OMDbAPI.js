1ea6d588d1762b0b09e0a21b7801df93f17007a7
sjschweber <schweber97@gmail.com>
2021-05-06 20:59:35 -0400

    Add dotenv

--- src/features/movie-search/OMDbAPI.js
+++ src/features/movie-search/OMDbAPI.js
@@ -1,7 +1,8 @@
 // A mock function to mimic making an async request for data
-//require('dotenv').config({path: '../../../.env'})
+require('dotenv').config({path: '../../../.env'})
 const axios = require('axios')
 
 export function fetchMovieTitles(movieTitle) {
+  console.log(process.env.REACT_APP_API_KEY)
   return axios.get(`http://www.omdbapi.com/?s=${movieTitle}&apikey=${process.env.REACT_APP_API_KEY}`)
 }
